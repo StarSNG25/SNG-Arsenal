d0059269e092094534e90ca546cab261b6a024a0
StarSNG25 <starpe.gamer@gmail.com>
2021-07-21 19:54:01 +0700

    Fixed armor rendering
    Removed getArmorModel, which makes it uses Minecraft's stock armor rendering

--- src/main/java/com/star_sng/sng_arsenal/armor/ModArmorItem.java
+++ src/main/java/com/star_sng/sng_arsenal/armor/ModArmorItem.java
@@ -1,8 +1,6 @@
 package com.star_sng.sng_arsenal.armor;
 
-import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
@@ -13,29 +11,6 @@ public class ModArmorItem extends ArmorItem
 	public ModArmorItem(IArmorMaterial armorMaterial, EquipmentSlotType sllotType, Properties properties)
 	{
 		super(armorMaterial, sllotType, properties);
-	}
-
-	@SuppressWarnings("unchecked")
-	public <A extends BipedModel<?>> A getArmorModel(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlotType slotType, A _default)
-	{
-		ModArmorModel model = new ModArmorModel(1.0f);
-		
-		model.Helmet.visible = slotType == EquipmentSlotType.HEAD;
-		model.Chestplate.visible = slotType == EquipmentSlotType.CHEST;
-		model.RightSleeve.visible = slotType == EquipmentSlotType.CHEST;
-		model.LeftSleeve.visible = slotType == EquipmentSlotType.CHEST;
-		model.RightLegging.visible = slotType == EquipmentSlotType.LEGS;
-		model.LeftLegging.visible = slotType == EquipmentSlotType.LEGS;
-		model.RightBoot.visible = slotType == EquipmentSlotType.FEET;
-		model.LeftBoot.visible = slotType == EquipmentSlotType.FEET;
-		
-		model.young = _default.young;
-		model.riding = _default.riding;
-		model.crouching = _default.crouching;
-		model.rightArmPose = _default.rightArmPose;
-		model.leftArmPose = _default.leftArmPose;
-		
-		return (A) model;
 	}
 	
 	public String getArmorTexture(ItemStack itemStack, Entity entity, EquipmentSlotType slotType, String type)
