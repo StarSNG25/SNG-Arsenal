b6c5764d0a4106ff2ce44e278307cc80a4394fc7
StarSNG25 <starpe.gamer@gmail.com>
2021-08-28 07:56:09 +0700

    Added condensed armor texture

--- src/main/java/com/star_sng/sng_arsenal/armor/ModArmorItem.java
+++ src/main/java/com/star_sng/sng_arsenal/armor/ModArmorItem.java
@@ -15,9 +15,17 @@ public class ModArmorItem extends ArmorItem
 	
 	public String getArmorTexture(ItemStack itemStack, Entity entity, EquipmentSlotType slotType, String type)
 	{
-		if (slotType == EquipmentSlotType.LEGS)
-			return "sng_arsenal:textures/models/armor/sng_layer_2.png";
+		if (material == ModArmorMaterial.SNG)
+			if (slotType == EquipmentSlotType.LEGS)
+				return "sng_arsenal:textures/models/armor/sng_layer_2.png";
+			else
+				return "sng_arsenal:textures/models/armor/sng_layer_1.png";
+		else if (material == ModArmorMaterial.CONDENSED_SNG)
+			if (slotType == EquipmentSlotType.LEGS)
+				return "sng_arsenal:textures/models/armor/condensed_sng_layer_2.png";
+			else
+				return "sng_arsenal:textures/models/armor/condensed_sng_layer_1.png";
 		else
-			return "sng_arsenal:textures/models/armor/sng_layer_1.png";
+			return null;
 	}
 }
