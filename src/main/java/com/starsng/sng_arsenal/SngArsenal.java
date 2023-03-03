bbe862d23afd8880feb19260f96ab1db8aeb54d3
StarSNG25 <starpe.gamer@gmail.com>
2023-03-03 15:52:00 +0700

    Removed unnecessary code

--- src/main/java/com/starsng/sng_arsenal/SngArsenal.java
+++ src/main/java/com/starsng/sng_arsenal/SngArsenal.java
@@ -1,12 +1,9 @@
 package com.starsng.sng_arsenal;
 
 import com.starsng.sng_arsenal.util.RegistryHandler;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(SngArsenal.MODID)
@@ -20,12 +17,5 @@ public class SngArsenal
 		
 		new RegistryHandler(modEventBus);
 		MinecraftForge.EVENT_BUS.register(this);
-	}
-	
-	@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-	public static class ClientModEvents
-	{
-		@SubscribeEvent
-		public static void onClientSetup(FMLClientSetupEvent event) {}
 	}
 }
